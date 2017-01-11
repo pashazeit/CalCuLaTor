c4476cf94152977283567962fc3b3917c2980ff0
Pasha Zeit <zeitlmd@gmail.com>
2017-01-11 13:08:49 +0300

    Add files via upload

--- src/main/java/Calcc/CalcControl.java
+++ src/main/java/Calcc/CalcControl.java
@@ -9,50 +9,48 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class CalcControl {
 
-    protected static final String A = "A";
-    protected static final String B = "B";
+   /* protected static final String A = "A";
+    protected static final String B = "B";*/
 
 
     @RequestMapping("plus")
-    public ModelAndView addd(HttpServletRequest request,
-                             HttpServletResponse response) throws Exception {
-        Integer numberA = Integer.parseInt(request.getParameter(A));
-        Integer numberB = Integer.parseInt(request.getParameter(B));
-        Integer result = numberA + numberB;
+    public ModelAndView getDetails(
+            @RequestParam(value = "A") int param1,
+            @RequestParam(value = "B") int param2) {
+        Integer result = param1 + param2;
+
         return new ModelAndView("index", "result", result.toString());
     }
 
-
     @RequestMapping("minus")
-    public ModelAndView subtract(HttpServletRequest request,
-                                 HttpServletResponse response) throws Exception {
-        Integer numberA = Integer.parseInt(request.getParameter(A));
-        Integer numberB = Integer.parseInt(request.getParameter(B));
-        Integer result = numberA - numberB;
+    public ModelAndView subtract(
+
+            @RequestParam(value = "A") int param1,
+            @RequestParam(value = "B") int param2) {
+        Integer result = param1 - param2;
         return new ModelAndView("index", "result", result.toString());
     }
 
     @RequestMapping("multiply")
-    public ModelAndView multiply(HttpServletRequest request,
-                                 HttpServletResponse response) throws Exception {
-        Integer numberA = Integer.parseInt(request.getParameter(A));
-        Integer numberB = Integer.parseInt(request.getParameter(B));
-        Integer result = numberA * numberB;
+    public ModelAndView multiply(
+            @RequestParam(value = "A") int param1,
+            @RequestParam(value = "B") int param2) {
+        Integer result = param1 * param2;
         return new ModelAndView("index", "result", result.toString());
     }
 
     @RequestMapping("divide")
-    public ModelAndView divide(HttpServletRequest request,
-                               HttpServletResponse response) throws Exception {
-        Integer numberA = Integer.parseInt(request.getParameter(A));
-        Integer numberB = Integer.parseInt(request.getParameter(B));
-        Integer result = numberA / numberB;
+    public ModelAndView divide(
+            @RequestParam(value = "A") int param1,
+            @RequestParam(value = "B") int param2) {
+        Integer result = param1 / param2;
         return new ModelAndView("index", "result", result.toString());
     }
 
